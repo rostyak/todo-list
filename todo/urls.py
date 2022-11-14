28e153710a037adf4d88800337415fbf13b03c25
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-14 14:59:05 +0200

    Update index page

--- todo/urls.py
+++ todo/urls.py
@@ -1,20 +1,26 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from todo.views import (
-    index,
+    IndexListView,
+    IndexCreateView,
+    IndexUpdateView,
+    IndexDeleteView,
     TagListView,
     TagCreateView,
     TagUpdateView,
-    TagDeleteView,
+    TagDeleteView, change_readiness,
 )
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", IndexListView.as_view(), name="index"),
+    path("create/", IndexCreateView.as_view(), name="index-create"),
+    path("<int:pk>/update/", IndexUpdateView.as_view(), name="index-update"),
+    path("<int:pk>/delete/", IndexDeleteView.as_view(), name="index-delete"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("<int:pk>/change-readiness/", change_readiness, name="change-readiness")
 ]
 
 app_name = "todo"
