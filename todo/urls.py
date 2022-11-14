d08c10281a3a4f49c37f9d5776c27ac5a47bd24c
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-14 12:25:52 +0200

    Create ordering for tasks, create page with tags

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from todo.views import index
+from todo.views import index, TagListView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list")
 ]
 
 app_name = "todo"
