350b528abd2e4944a2970b28e48c876715ebc102
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-14 14:59:05 +0200

    Update index page

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-from todo.models import Tag
+from todo.models import Tag, Task
 
 
-def index(request):
-    return render(request, "todo/index.html")
+class IndexListView(generic.ListView):
+    model = Task
+    template_name = "todo/index.html"
+
+
+class IndexCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:index")
+
+
+class IndexUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:index")
+
+
+class IndexDeleteView(generic.DeleteView):
+    model = Task
+    success_url = "todo/index.html"
 
 
 class TagListView(generic.ListView):
@@ -29,3 +47,10 @@ class TagUpdateView(generic.UpdateView):
 class TagDeleteView(generic.DeleteView):
     model = Tag
     success_url = reverse_lazy("todo:tag-list")
+
+
+def change_readiness(request, pk):
+    task = Task.objects.get(id=pk)
+    task.readiness = not task.readiness
+    task.save()
+    return redirect("todo:index")
