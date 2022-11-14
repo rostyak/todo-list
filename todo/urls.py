dfa560f79a9977e9d29cd4394b9c55a163590294
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-11-14 12:52:44 +0200

    Create create/ipdate/delete views for tags

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from todo.views import index, TagListView
+from todo.views import (
+    index,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tags/", TagListView.as_view(), name="tag-list")
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Tag
@@ -11,3 +12,20 @@ def index(request):
 class TagListView(generic.ListView):
     model = Tag
     template_name = "todo/tag_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
